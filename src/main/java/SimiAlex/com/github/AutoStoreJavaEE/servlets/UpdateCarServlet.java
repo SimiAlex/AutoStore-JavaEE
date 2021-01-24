ff1b46d7a95b4b760dbf12999a770c1960dc8bfa
remcv <remuluscatana@tutanota.com>
2021-01-24 15:20:12 +0200

    Fixed UpdateCarServlet to use DIRepositories

--- src/main/java/SimiAlex/com/github/AutoStoreJavaEE/servlets/UpdateCarServlet.java
+++ src/main/java/SimiAlex/com/github/AutoStoreJavaEE/servlets/UpdateCarServlet.java
@@ -11,26 +11,30 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import SimiAlex.com.github.AutoStoreJavaEE.entities.Car;
-import SimiAlex.com.github.AutoStoreJavaEE.repository.CarRepository;
+import SimiAlex.com.github.AutoStoreJavaEE.repository.ItemRepository;
+import SimiAlex.com.github.AutoStoreJavaEE.utilities.DIRepositories;
 
 @WebServlet(name = "UpdateCarServlet", urlPatterns = "/update-car")
-public class UpdateCarServlet extends HttpServlet {
-    
-    
+public class UpdateCarServlet extends HttpServlet 
+{
+    // methods    
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        
-        //recover car id from request
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException 
+    {    
+        // recover car id from request
         int carId = Integer.parseInt(req.getParameter("carId"));
+       
+        // recover a CarRepositoryImpl instance from the application scope
         ServletContext application = getServletContext();
-        CarRepository cr = (CarRepository) application.getAttribute("carRepoImpl");
+        ItemRepository<Car> carRepo = DIRepositories.getCarRepository(application);
 
-        //recover car from database
-        Car myCar = cr.findById(carId);
+        // recover car from database
+        Car myCar = carRepo.findById(carId);
         
-        //save to req atribute
+        // save car to req atribute
         req.setAttribute("myCar", myCar);
 
+        // dispatch the request to the corresponding jsp page
         RequestDispatcher dispatcher = req.getRequestDispatcher("update-car-form.jsp");
         dispatcher.forward(req, resp);
     }
